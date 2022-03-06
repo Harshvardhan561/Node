e2830b3d2c0b897acaedfdf708956253309fd247
Harshvardhan561 <harshvardhanarra@gmail.com>
2022-03-06 23:53:41 +0530

    file handling 3

--- fs.js
+++ fs.js
@@ -1,8 +1,13 @@
 //fs -> file system module
 const fs = require("fs");
+//console.log(fs);
 //const f1 = require("./f1.txt");
-// const abc = require("../js/temp");  // to access temp file in js folder
-// console.log(abc);
+ const abc = require("../js/temp");  // to access temp file in js folder
+// require method , goes to the file that is needed to be required . executes that file. And if there is something that is exported we get that in variable 'abc'
+
+ console.log(abc);
+ let ans = abc.add(45,4);
+ console.log(ans);
 
 
 //appendilesync appends data into file, if file is not present  , it creates the file and then appends the data.
@@ -13,7 +18,8 @@ const fs = require("fs");
 
 //console.log(res);
 //let data = fs.readFileSync("f1.txt");
-// console.log(data);
+//data recieved is an object type ,data is in buffer format. for it to be readable , we convert it from buffer to string .
+// console.log(data  + ""); //automatic type conversion se buffer , string mein covert ho gayi hai
 
 // let data = fs.readFileSync("f1.txt","utf-8");
 // console.log(data);
