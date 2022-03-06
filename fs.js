634470648328b24a9b31a219f6015fc96a2d92fa
Harshvardhan561 <harshvardhanarra@gmail.com>
2022-03-06 23:21:41 +0530

    node using require to access temp file in js folder 2

--- fs.js
+++ fs.js
@@ -1,18 +1,18 @@
 //fs -> file system module
 const fs = require("fs");
-const f1 = require("./f1.txt");
-const abc = require("../js/temp");
-console.log(abc);
+//const f1 = require("./f1.txt");
+// const abc = require("../js/temp");  // to access temp file in js folder
+// console.log(abc);
 
 
 //appendilesync appends data into file, if file is not present  , it creates the file and then appends the data.
-let res = fs.appendFileSync("f1.txt","Hello i am f2 file");
-fs.appendFileSync("f1.txt","\nYou guys are smart");
+// let res = fs.appendFileSync("f1.txt","Hello i am f2 file");
+// fs.appendFileSync("f1.txt","\nYou guys are smart");
 
 //console.log(res);
 //let data = fs.readFileSync("f1.txt");
 // console.log(data);
 
-let data = fs.readFileSync("f1.txt","utf-8");
-console.log(data);
+// let data = fs.readFileSync("f1.txt","utf-8");
+// console.log(data);
 
